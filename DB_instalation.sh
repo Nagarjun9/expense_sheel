983e0d71faed7e42177343e2e6ff75f279f9a306
Nagarjun9 <nagunaggu@gmail.com>
2024-04-21 07:47:29 +0530

    function

--- DB_instalation.sh
+++ DB_instalation.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPTNAME=$(echo $0 |cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log 
-G="e[31m"
-R="e\[32m"
-Y="e\[33m"
-N="e\[0m"
+G="\e[31m"
+R="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 VALIDATION(){
     if [ $1 -ne 0 ]
     then 
