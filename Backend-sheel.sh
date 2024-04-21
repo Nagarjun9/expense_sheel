f7f0f98b9c6fcd7fe407fb4979a0b0ab1bcdadf0
Nagarjun9 <nagunaggu@gmail.com>
2024-04-21 11:15:28 +0530

    function

--- Backend-sheel.sh
+++ Backend-sheel.sh
@@ -2,6 +2,12 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%s)
 SCRIPTNAME=$(echo $0 |cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPTNAME-TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
 
 echo "please the password"
 read "my_root_password"
