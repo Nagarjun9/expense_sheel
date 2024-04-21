3495957a22da8e74af284e6eaaaa1b37ae8a9146
Nagarjun9 <nagunaggu@gmail.com>
2024-04-21 07:45:14 +0530

    function

--- DB_instalation.sh
+++ DB_instalation.sh
@@ -1,8 +1,13 @@
 #!/bib/bash
 
 USERID=$(id -u)
-
-
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log 
+G="e[31m"
+R="e\[32m"
+Y="e\[33m"
+N="e\[0m"
 VALIDATION(){
     if [ $1 -ne 0 ]
     then 
@@ -15,11 +20,11 @@ VALIDATION(){
 
 if [ $USERID -ne 0 ]
 then 
-    echo " you are a not super user please use root crediantials"
+    echo -d "$R you are a not super user please use root crediantials $N"
     exit 1
 else 
     echo "you are a superuser"
 fi
 
-dnf install mysql-server -y
+dnf install mysql-server -y &>>$LOGFILE
 VALIDATION $? "instalation of mysql"
